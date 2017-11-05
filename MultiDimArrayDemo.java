65afaff8a91574feab39bbea55f860d9cba28aee
Administrator <Administrator@User-PC>
2017-11-05 15:29:27 +0000

    comments added

--- MultiDimArrayDemo.java
+++ MultiDimArrayDemo.java
@@ -1,8 +1,13 @@
 package arraydemo;
 
+/*
+ * This is array demo class
+ */
 public class MultiDimArrayDemo
 {
-
+	/*
+	 * this is main method
+	 */
 	public static void main(String[] s)
 	{
 		System.out.println("Executing MultiDimArrayDemo");
